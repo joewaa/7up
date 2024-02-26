35a9c9290f89b75049a3def63731ac5ea353a46c
EC2 Default User <ec2-user@ip-100-64-222-206.ap-east-1.compute.internal>
2024-02-26 04:04:53 +0000

    appProperties for configmap update

--- application/helm-chart/test.sh
+++ application/helm-chart/test.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-#helm install --debug --dry-run test-app . \
-helm template . \
+#helm install --debug --dry-run test-app . --values=values.yaml \
+helm template demo  . \
   --namespace demo \
-  --set volumeMounts[0].name=foo \
-  --set volumeMounts[0].mountPath="/etc/foo" \
-  --set volumeMounts[0].readOnly=true \
-  --set volumes[0].name=foo \
-  --set volumes[0].configMap.name=mychart-configmap \
-  --set containerConfig."application-app-prd\.properties"='
+  --set appProperties."/apps/prod/application-app-prd\.properties"='
   logging.config=config/log4j2.properties
   #eureka.instance.hostname=${HOSTNAME}
   spring.cloud.client.hostname=${HOSTNAME}
@@ -16,5 +11,18 @@ helm template . \
   spring.application.name=api-gateway
   server.port=8080
   ' \
+  --set appProperties."/apps/dev/dev\.properites"='
+  helloworld
+  '  
+  # --set volumeMounts[0].name=foo \
+  # --set volumeMounts[0].mountPath="/etc/foo" \
+  # --set volumeMounts[0].readOnly=true \
+  # --set volumes[0].name=foo \
+  # --set volumes[0].configMap.name=demo-config \
   
   
+  #--set podAnnotations.configchecksum="{{ include (print .Template.BasePath '/configmap.yaml') . | sha256sum | trunc 63 }}"
+  
+        #{{- if .Values.appProperties }}
+        #configchecksum: {{ include (print .Template.BasePath "/configmap.yaml") . | sha256sum | trunc 63 }}
+        #{{- end }}
